10685326d72d695bfb3c84763b48093daa85617a
Sergio Guillen <chejoguillen@gmail.com>
2019-10-25 23:19:39 -0600

    servicio
    evitar bloqueo de cors

--- proyecto/servicios/proyecto/src/main/java/proyecto/rsc/ProductoRsc.java
+++ proyecto/servicios/proyecto/src/main/java/proyecto/rsc/ProductoRsc.java
@@ -3,6 +3,7 @@ package proyecto.rsc;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import proyecto.ctrl.ProductoCtrl;
 import proyecto.ent.Producto;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping (path="/productos")
 public class ProductoRsc {
